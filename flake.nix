d28ce9db6a11453c27a756292244a6b6575b1727
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-07-01 11:05:18 +0200

    Update Obelisk to 0.39.4

--- flake.nix
+++ flake.nix
@@ -2,27 +2,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
     obelisk = {
-      url = "github:obeli-sk/obelisk/latest-pre";
+      url = "github:obeli-sk/obelisk/latest";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
-        rust-overlay.follows = "rust-overlay";
       };
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, obelisk }:
+  outputs = { self, nixpkgs, flake-utils, obelisk }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
+          pkgs = import nixpkgs { inherit system ; };
           commonDeps = with pkgs; [
             socat
             nodejs_22
