c5f5377049ecded55626170090080180c61ea9bf
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-07-06 08:28:42 +0200

    build: Add `dev-deps`

---
+++ scripts/dev-deps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Collect versions of binaries installed by `nix develop` producing file `dev-deps.txt`.
+# This script should be executed after every `nix flake update`.
+
+set -exuo pipefail
+cd "$(dirname "$0")/.."
+
+rm -f dev-deps.txt
+obelisk --version >> dev-deps.txt
