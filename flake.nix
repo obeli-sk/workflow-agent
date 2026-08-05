e48b7eb5d277cf3d06a9dce1b9c8816194756ca9
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-08-05 22:43:22 +0200

    build: Update to obelisk 0.41.0

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       };
     };
     obelisk = {
-      url = "github:obeli-sk/obelisk/latest-pre";
+      url = "github:obeli-sk/obelisk/latest";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
